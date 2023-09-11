9fe6e6e9543d1bde4067fe256e567e3ef8b27d92
jason <jason.aghedo@gmail.com>
2023-09-11 22:58:56 +0100

    Completed Project

---
+++ index.js
@@ -0,0 +1,9 @@
+const currentDayOfTheWeek = document.querySelector(`[data-testid="currentDayOfTheWeek"]`)
+const currentUTCTimeElement = document.querySelector(`[data-testid="currentUTCTime"]`);
+setInterval(() => {
+    const date = new Date();
+    const currentTime = date.getTime();
+    const currentDay = date.getDay();
+    currentUTCTimeElement.textContent = currentTime;
+    currentDay.textContent = currentTimeMillis;
+}, 1000);
